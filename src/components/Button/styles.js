77986bc822337c1972f705c2625bbd06092db410
Arnau Fugarolas Barbena <arnau.fugarolas@mataro.epiaedu.cat>
2023-01-17 11:49:58 +0100

    Minor fixes
    Signed-off-by: Arnau Fugarolas Barbena <arnau.fugarolas@mataro.epiaedu.cat>

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled'
 
 export const Button = styled.button`
+  width: ${props => props.width};
+  height: ${props => props.height};
+  border: none;
+  background-color: transparent;
   background-image: url(${props => props.image});
   background-position: center;
   background-repeat: no-repeat;
   background-size: 100%;
-  background-color: transparent;
-  border: none;
-  width: ${props => props.width};
-  height: ${props => props.height};
-  `
+`
