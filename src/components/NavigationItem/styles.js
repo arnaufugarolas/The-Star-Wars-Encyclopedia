ecf85237a5a37ed8634f47d086ef8540f2ed6b8c
EricBM19 <ericbm19@gmail.com>
2023-01-17 11:15:18 +0100

    style updated

--- src/components/NavigationItem/styles.js
+++ src/components/NavigationItem/styles.js
@@ -1,6 +1,19 @@
 import styled from '@emotion/styled'
 
 export const MenuItem = styled.a`
-color: FFE81F;
-font-family: Arvo;
+width: ${props => props.width};
+height: ${props => props.height};
+align-items: center;
+justify-content: center;
+color: ${props => props.textColor};
+font-family: ${props => props.fontFamily};
+font-size: ${props => props.fontSize};
 `
+
+MenuItem.defaultProps = {
+  width: '15rem',
+  height: '5rem',
+  textColor: 'var(--clr-yellow)',
+  fontFamily: 'var(--ff-primary-body)',
+  fontSize: '1.5rem'
+}
