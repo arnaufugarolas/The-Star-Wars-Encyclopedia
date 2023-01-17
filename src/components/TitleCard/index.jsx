b329da61ee64d57a0276e8ce11e7dec8d37a8ef6
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2023-01-17 11:01:09 +0100

    Finish
    Signed-off-by: arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>

--- src/components/TitleCard/index.jsx
+++ src/components/TitleCard/index.jsx
@@ -1,5 +1,23 @@
-export const Index = () => {
+import { TitleCard } from './styles'
+import React from 'react'
+
+export const Index = (props) => {
+  const { title, onClickAction } = props
+
+  const clickHandler = () => {
+    onClickAction()
+  }
+
   return (
-    <></>
+    <TitleCard onClick={clickHandler}>
+      {title}
+    </TitleCard>
   )
 }
+
+Index.defaultProps = {
+  title: 'Title',
+  onClickAction: {}
+}
+
+export default Index
