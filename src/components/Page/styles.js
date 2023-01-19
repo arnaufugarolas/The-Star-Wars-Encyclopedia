6715afb9bbe66a97466b18579b1a32f9f2f6acb0
Arnau Fugarolas Barbena <arnau.fugarolas@mataro.epiaedu.cat>
2023-01-19 21:25:50 +0100

    WIP
    Signed-off-by: Arnau Fugarolas Barbena <arnau.fugarolas@mataro.epiaedu.cat>

--- src/components/Page/styles.js
+++ src/components/Page/styles.js
@@ -1,7 +1,1 @@
-import styled from '@emotion/styled'
-
-export const BackgroundColor = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: #17184E;
-`
+// import styled from '@emotion/styled'
