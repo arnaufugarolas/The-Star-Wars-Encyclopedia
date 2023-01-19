076f78839fafdab4f4bc56615904bfc5222edfa3
EricBM19 <ericbm19@gmail.com>
2023-01-19 17:01:27 +0100

    Adding Title Card

--- src/components/ItemPage/ItemPage.jsx
+++ src/components/ItemPage/ItemPage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import NavigationBar from '../NavigationBar/NavigationBar'
+import TitleCard from '../TitleCard/index'
 import { BackgroundColor } from './styles'
 
 const ItemPage = () => {
   return (
     <BackgroundColor>
       <NavigationBar />
+      <div>
+        <TitleCard />
+      </div>
     </BackgroundColor>
   )
 }
