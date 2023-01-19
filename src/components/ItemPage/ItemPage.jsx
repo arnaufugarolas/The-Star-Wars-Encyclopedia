7c4697eefc43b3e42fc0f759eb56a7596eb4ec9c
EricBM19 <ericbm19@gmail.com>
2023-01-19 16:58:25 +0100

    First ItemPage commit

---
+++ src/components/ItemPage/ItemPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavigationBar from '../NavigationBar/NavigationBar'
+import { BackgroundColor } from './styles'
+
+const ItemPage = () => {
+  return (
+    <BackgroundColor>
+      <NavigationBar />
+    </BackgroundColor>
+  )
+}
+
+export default ItemPage
---
+++ src/components/ItemPage/styles.js
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled'
+
+export const BackgroundColor = styled.div`
+ background-color: #17184E;
+ width: 100vw;
+ height: 100vh;
+`
