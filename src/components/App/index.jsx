42ffd79a161d6cc82026e0eaf094d4b3041a44b9
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2023-01-19 16:54:48 +0100

    Finish DataCard
    Signed-off-by: arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,16 +1,8 @@
 import React from 'react'
-import DataCard from '../DataCard'
-
-const data = [
-  { title: 'Name', content: 'John Doe', redirect: '/profile' },
-  { title: 'Age', content: '25' }
-]
 
 function Index () {
   return (
-    <div className='App'>
-      <DataCard data={data} title='Probe' />
-    </div>
+    <div className='App' />
   )
 }
 
--- src/components/DataItem/index.jsx
+++ src/components/DataItem/index.jsx
@@ -1,15 +1,35 @@
 import React from 'react'
-import { BoltText, DataContainer, RedirectionLink } from './styles'
+import {
+  BoltText,
+  DataContainer,
+  LinkText,
+  NormalText,
+  RedirectionLink
+} from './styles'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-function Index ({ title, content, redirect }) {
+function Index (props) {
+  const { title, content, redirect } = props
+
   return (
     <DataContainer>
-      <BoltText>{title}: </BoltText>
+      <BoltText>{title}:</BoltText>
       {redirect
-        ? <RedirectionLink href={redirect}>{content}</RedirectionLink>
-        : <p>{content}</p>}
+        ? <Router>
+          <RedirectionLink to={redirect}>
+            <LinkText>{content}</LinkText>
+          </RedirectionLink>
+        </Router> // eslint-disable-line react/jsx-closing-tag-location
+        : <NormalText>{content}</NormalText>}
     </DataContainer>
   )
+}
+
+Index.defaultProps = {
+  className: 'data-item',
+  title: 'Title',
+  content: 'Content',
+  redirect: undefined
 }
 
 export default Index
