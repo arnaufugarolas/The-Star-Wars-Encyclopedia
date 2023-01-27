afae157b240505346452c47c609d7005ddc63999
arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>
2023-01-27 11:24:50 +0100

    WIP CategoryPage
    Signed-off-by: arnaufugarolas <arnau.fugarolas@mataro.epiaedu.cat>

--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -1,37 +1,20 @@
 import NavigationBar from '../NavigationBar/index.jsx'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useParams } from 'react-router-dom'
 
 export const Index = (props) => {
   const { children } = props
   const { language } = useParams()
-  const [loading, setLoading] = useState(false)
 
-  const getRoutes = () => {
-    return [
-      { route: `/${language}/`, pageName: 'home' },
-      { route: `/${language}/people/`, pageName: 'people' },
-      { route: `/${language}/films/`, pageName: 'films' },
-      { route: `/${language}/species/`, pageName: 'species' },
-      { route: `/${language}/vehicles/`, pageName: 'vehicles' },
-      { route: `/${language}/starships/`, pageName: 'starships' },
-      { route: `/${language}/planets/`, pageName: 'planets' }
-    ]
-  }
-  const [routes, setRoutes] = useState(getRoutes())
-
-  useEffect(() => {
-    if (!loading) {
-      setRoutes([])
-      setLoading(true)
-    }
-  }, [language])
-
-  useEffect(() => {
-    if (loading && routes.length === 0) {
-      setRoutes(getRoutes())
-    }
-  })
+  const routes = [
+    { route: `/${language}/`, pageName: 'home' },
+    { route: `/${language}/people/`, pageName: 'people' },
+    { route: `/${language}/films/`, pageName: 'films' },
+    { route: `/${language}/species/`, pageName: 'species' },
+    { route: `/${language}/vehicles/`, pageName: 'vehicles' },
+    { route: `/${language}/starships/`, pageName: 'starships' },
+    { route: `/${language}/planets/`, pageName: 'planets' }
+  ]
 
   return (
     <>
