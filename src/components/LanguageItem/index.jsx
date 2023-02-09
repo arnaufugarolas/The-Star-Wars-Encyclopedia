05161586049e37266af871183123da2a21ed3049
Balsalobre martinez, Eric <e.balsalobremartin@travelport.com>
2023-02-09 11:31:15 +0100

    clicable text

--- src/components/LanguageItem/index.jsx
+++ src/components/LanguageItem/index.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import { LanguageText } from './styles'
 
 const Index = ({ languages }) => {
+  const handleClick = language => {
+    console.log(`Language: ${language}`)
+  }
   return (
     <ul>
       {languages.map((language, index) => (
-        <LanguageText key={index}>{language}</LanguageText>
+        <li key={index}>
+          <div onClick={() => handleClick(language)}>
+            <LanguageText>{language}</LanguageText>
+          </div>
+        </li>
       ))}
     </ul>
   )
