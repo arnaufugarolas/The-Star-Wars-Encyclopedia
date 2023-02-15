4539e8b11454be77630d12526f4f6005d4cdebaa
Balsalobre martinez, Eric <e.balsalobremartin@travelport.com>
2023-02-15 12:11:20 +0100

    LanguageItem Almost Done finally working

--- src/components/LanguageItem/index.jsx
+++ src/components/LanguageItem/index.jsx
@@ -1,17 +1,52 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { LanguageText } from './styles'
 
+const languageCodes = {
+  wo: 'Wookiee',
+  bg: 'Bulgarian',
+  cs: 'Czech',
+  da: 'Danish',
+  de: 'German',
+  el: 'Greek',
+  es: 'Spanish',
+  et: 'Estonian',
+  fi: 'Finnish',
+  fr: 'French',
+  hu: 'Hungarian',
+  id: 'Indonesian',
+  it: 'Italian',
+  ja: 'Japanese',
+  ko: 'Korean',
+  lt: 'Lithuanian',
+  lv: 'Latvian',
+  nb: 'Norwegian',
+  nl: 'Dutch',
+  pl: 'Polish',
+  br: 'Portuguese (Brazilian)',
+  pt: 'Portuguese',
+  ro: 'Romanian',
+  ru: 'Russian',
+  sk: 'Slovak',
+  sl: 'Slovenian',
+  sv: 'Swedish',
+  tr: 'Turkish',
+  uk: 'Ukrainian',
+  zh: 'Chinese (simplified)'
+}
+
+const getLanguageCode = (name) => {
+  return Object.keys(languageCodes).find((key) => languageCodes[key] === name) || 'en'
+}
+
 const Index = ({ languages }) => {
-  const handleClick = language => {
-    console.log(`Language: ${language}`)
-  }
   return (
     <ul>
       {languages.map((language, index) => (
         <li key={index}>
-          <div onClick={() => handleClick(language)}>
+          <Link to={`/${getLanguageCode(language)}`}>
             <LanguageText>{language}</LanguageText>
-          </div>
+          </Link>
         </li>
       ))}
     </ul>
